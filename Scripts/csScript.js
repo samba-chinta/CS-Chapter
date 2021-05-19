61a6532e796f9ddf3a2f58977e4d94546fce2551
samba <74446747+samba-chinta@users.noreply.github.com>
2021-05-19 23:19:04 +0530

    Update csScript.js

--- Scripts/csScript.js
+++ Scripts/csScript.js
@@ -6,13 +6,10 @@ let bg_img = document.getElementById("bg-wallpaper");
 let bg_video = document.getElementById("bg-video");
 let cs_video = document.getElementById("cs-video");
 
-// Arrays for the image gallery holding different images.
 let imgArrOne = ['Assets/pic-1.jpg', 'Assets/pic-2.png'];
 let imgArrTwo = ['Assets/pic-3.jpg', 'Assets/pic-4.jpg'];
 let imgArrThree = ['Assets/pic-5.jpg', 'Assets/pic-6.jpg'];
 
-
-// Variables and function set for animation in gallery.
 let countOne = 0,
     countTwo = 0,
     countThree = 0;
@@ -20,15 +17,12 @@ let func1 = setInterval(animeOne, 3000);
 let func2 = setInterval(animeTwo, 2000);
 let func3 = setInterval(animeThree, 4000);
 
-
-// menu_closing function
 menu_close.addEventListener('click', function()
 {
     menu_open.style.display = "block";
     menu_close.style.display = "none";
 });
 
-// menu_opening function
 menu_open.addEventListener('click', function()
 {
     menu_open.style.display = "none";
@@ -36,7 +30,6 @@ menu_open.addEventListener('click', function()
 
 });
 
-// onmouseover event to embed the youtube link to iframe
 bg_img.onmouseover = () => {
     bg_img.style.zIndex = "-1";
     bg_video.style.zIndex = "1";
@@ -44,14 +37,12 @@ bg_img.onmouseover = () => {
     "https://www.youtube.com/embed/lPAAlbd27rs?controls=0";
 }
 
-//onmouseout event to remove embeded link from iframe
 bg_video.onmouseout = () => {
     bg_img.style.zIndex = "1";
     bg_video.style.zIndex = "-1";
     cs_video.src = null;
 }
 
-//these 3 functions are for image animations
 function animeOne() {
     document.getElementById('img-1').src = imgArrOne[countOne];
     countOne += 1;
@@ -71,15 +62,6 @@ function animeThree() {
     countThree += 1;
     if (countThree >= imgArrThree.length)
         countThree = 0;
-}
-
-// This is for creating scroll spy using onscroll event
-window.onscroll = () => {
-    let scroll_pos = window.pageYOffset;
-    let bar = document.getElementById("spyBar");
-    const OFFSET = window.innerHeight / window.innerWidth;
-
-    bar.style.width = (OFFSET*scroll_pos).toString() + "px";
 }
 
 window.onscroll = () => 
